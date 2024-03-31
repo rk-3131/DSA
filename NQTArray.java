331a00037d26883f809ecdd729642c65ca8c10b3
Radhakrushna Mahadik <mahdikrs512@gmail.com>
2024-03-31 21:31:49 +0530

    Rotating the array

--- NQTArray.java
+++ NQTArray.java
@@ -5,11 +5,11 @@ import java.util.*;
 public class NQTArray {
     public static void main(String[] args) {
 //        int[] arr = {9, 4, 5, 6, 3, 1, 7, 5, 60, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 100, 60};
-        int [] arr = {5,3,2,1,4};
+//        int [] arr = {5,3,2,1,4};
 //        System.out.println(minNumber(arr));
 //        System.out.println(maxNumber(arr));
 
-        System.out.println(Arrays.toString(arr));
+//        System.out.println(Arrays.toString(arr));
 //        System.out.println(Arrays.toString(secondSmallestSecondLargest(arr)));
 //        System.out.println(Arrays.toString(secondSmallestSecondLargest2(arr)));
 //        reverseArray(arr);
@@ -26,30 +26,36 @@ public class NQTArray {
 //        System.out.println(Arrays.toString(arr));
 
 //        System.out.println(sumOfElements(arr));
-        rotateByGivenValue(arr, 3);
-        System.out.println(Arrays.toString(arr));
+//        rotateByGivenValue(arr, 3);
+//        System.out.println(Arrays.toString(arr));
 
+        int [] arr = {1,2,3,4,5,6,7,8,9};
+        System.out.println(Arrays.toString(arr));
+        rotateByGivenValue(arr, 2);
+        System.out.println(Arrays.toString(arr));
     }
 
     static void rotateByGivenValue(int [] arr, int k){
 //        1 2 3 4 5 6 7 8 9
-        List<Integer> list = new ArrayList<Integer>();
 
-        for (int i = 0; i < arr.length; i++){
-            list.add(arr[i]);
-        }
-        int index = arr.length - 1;
-        int lastIndex = index - k + 1;
+        int [] temp = new int[arr.length];
+        int index = k;
 
-        while (index != lastIndex){
-            int number = list.remove(index);
-            list.add(0, number);
-            index--;
+        for (int i = 0; i < arr.length - k; i++){
+            temp[index] = arr[i];
+            index++;
         }
 
-        for (int i = 0; i < list.size(); i++){
-            arr[i] = list.get(i);
+        index = 0;
+        for (int i = arr.length - k; i < arr.length; i++){
+            temp[index] = arr[i];
+            index++;
         }
+
+        for (int i = 0; i < temp.length; i++){
+            arr[i] = temp[i];
+        }
+
     }
 
     static int sumOfElements(int [] arr){
